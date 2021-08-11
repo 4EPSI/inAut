054ff808fae2420ebb7737ae8351bd166486e69d
4EPSI <mnatsakanyan@yual.io>
2021-08-11 15:29:59 +0300

    angebot content

--- app/js/main.js
+++ app/js/main.js
@@ -11,3 +11,15 @@ $('.customer-slider').slick({
 $('.customNextBtn').on('click', function() {
     $('.customer-slider').slick('slickNext');
 });
+
+// // our-customer-slider
+// $('.our-customer-slider').slick({
+//     dots: true,
+//     infinite: true,
+//     speed: 300,
+//     slidesToShow: 2,
+//     // centerMode: true,
+//     arrows: true,
+//     variableWidth: true,
+//     draggable: false
+// });
