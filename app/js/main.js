9420b8ad02498bd61bde0cf21de71c8da75eab2b
4EPSI <mnatsakanyan@yual.io>
2021-08-11 03:27:20 +0300

    home content

---
+++ app/js/main.js
@@ -0,0 +1,13 @@
+$('.customer-slider').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 2,
+    // centerMode: true,
+    arrows: true,
+    variableWidth: true,
+    draggable: false
+});
+$('.customNextBtn').on('click', function() {
+    $('.customer-slider').slick('slickNext');
+});
