e43ba15887b7f4999380748423d3b25c97378c3d
4EPSI <mnatsakanyan@yual.io>
2021-08-12 22:14:29 +0300

    slider

--- app/js/main.js
+++ app/js/main.js
@@ -13,20 +13,38 @@ $(document).on('click', function (e) {
     e.stopPropagation();
 });
 
-
-$('.customer-slider').slick({
-    dots: true,
-    infinite: true,
+// swiper
+var swiper = new Swiper(".mySwiper", {
+    pagination: {
+      el: ".swiper-pagination",
+      type: "fraction",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev"
+    },
+    simulateTouch: false,
+    loop: true,
     speed: 300,
-    slidesToShow: 2,
-    // centerMode: true,
-    arrows: true,
-    variableWidth: true,
-    draggable: false
 });
-$('.customNextBtn').on('click', function() {
-    $('.customer-slider').slick('slickNext');
-});
+
+
+// $('.customer-slider').slick({
+//     dots: true,
+//     infinite: true,
+//     speed: 300,
+//     // slidesToShow: 2,
+//     // centerMode: true,
+//     arrows: true,
+//     variableWidth: true,
+//     draggable: false,
+//     nextArrow: '<img src="img/svg/arrow_right.svg">',
+//     prevArrow: '<img src="img/svg/arrow_left.svg">'
+// });
+// $('.customNextBtn').on('click', function() {
+//     $('.customer-slider').slick('slickNext');
+// });
 
 // // our-customer-slider
 // $('.our-customer-slider').slick({
