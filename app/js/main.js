5703dcdaf6f2ae80614c3406f295423859bff191
4EPSI <mnatsakanyan@yual.io>
2021-08-16 19:00:13 +0300

    mob

--- app/js/main.js
+++ app/js/main.js
@@ -32,19 +32,19 @@ $('.swiper-dots').on('click', '.swiper-dot', function() {
 });
 
 
-// var swiper = new Swiper(".mySwiper2", {
-//   slidesPerView: 1,
-//   spaceBetween: 30,
-//   loop: true,
-//   pagination: {
-//     el: ".swiper-pagination",
-//     clickable: true,
-//   },
-//   navigation: {
-//     nextEl: ".swiper-button-next",
-//     prevEl: ".swiper-button-prev",
-//   },
-// });
+var swiper2 = new Swiper(".mySwiper2", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  },
+});
 
 // $('.customer-slider').slick({
 //     dots: true,
