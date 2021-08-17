dbad0abae3233b7bbb8e7dc415e01751cbbec91e
4EPSI <mnatsakanyan@yual.io>
2021-08-17 15:28:15 +0300

    slider fix page for-companies

--- app/js/main.js
+++ app/js/main.js
@@ -60,6 +60,23 @@ var swiper3 = new Swiper(".mySwiper3", {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 1
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 1,
+      spaceBetween: 1
+    },
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 3,
+      spaceBetween: 1
+    }
+  }
 });
 
 
